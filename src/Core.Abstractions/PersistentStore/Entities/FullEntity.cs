46271023badedb897a1b9c64dc383cb410fc932c
MaiReo <sawako.saki@gmail.com>
2018-08-24 11:13:12 +0800

    添加实体审计抽象

--- src/Core.Abstractions/PersistentStore/Entities/FullEntity.cs
+++ src/Core.Abstractions/PersistentStore/Entities/FullEntity.cs
@@ -5,16 +5,18 @@ using System.Text;
 namespace Core.PersistentStore
 {
 
-    public abstract class FullEntity : FullEntity<int>, IHasCreationTime, IHasModificationTime, ISoftDelete, IEntity<int>, IEntity, IEntityBase
+    public abstract class FullEntity : FullEntity<int>, IHasCreationTime, IHasModificationTime, IHasDeletionTime, ISoftDelete, IEntity<int>, IEntity, IEntityBase
     {
     }
 
-    public abstract class FullEntity<T> : Entity<T>, IHasCreationTime, IHasModificationTime, ISoftDelete, IEntity<T>, IEntityBase
+    public abstract class FullEntity<T> : Entity<T>, IHasCreationTime, IHasModificationTime, IHasDeletionTime, ISoftDelete, IEntity<T>, IEntityBase
     {
-        public DateTimeOffset CreationTime { get; set; }
+        public virtual DateTimeOffset CreationTime { get; set; }
 
-        public DateTimeOffset? LastModificationTime { get; set; }
+        public virtual DateTimeOffset? LastModificationTime { get; set; }
 
         public bool IsDeleted { get; set; }
+
+        public virtual DateTimeOffset? DeletionTime { get; set; }
     }
 }
